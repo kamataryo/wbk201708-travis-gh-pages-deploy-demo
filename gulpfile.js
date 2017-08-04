19ccef1d253636352363f93b1e95d472e036e296
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2017-08-04 11:35:31 +0900

    Init repo

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+
+gulp.task('style', () => gulp
+  .src('./src/styles/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('./dist'))
+)
+
+
+gulp.task('html', () => gulp
+  .src('./src/index.html')
+  .pipe(gulp.dest('./dist'))
+)
+
+gulp.task('default', ['style', 'html'])
